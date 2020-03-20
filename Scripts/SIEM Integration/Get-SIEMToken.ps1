a15720ccaa94a0c0f129d3459d0d341ad4b4f498
JesseEsquivel <33558203+JesseEsquivel@users.noreply.github.com>
2020-03-20 16:06:07 -0400

    Create Get-SIEMToken.ps1

---
+++ Scripts/SIEM Integration/Get-SIEMToken.ps1
@@ -0,0 +1,47 @@
+<#
+##################################################################################################################
+#
+# Microsoft
+# Get-SIEMToken.ps1
+# jesse.esquivel@microsoft.com
+# -sample code taken from MDATP-Hello-World and modified:
+#  https://docs.microsoft.com/en-us/windows/security/threat-protection/microsoft-defender-atp/api-hello-world
+# -Use to test your MDATP SIEM API for integration, run this first and then run Get-SIEMAlerts
+# 
+# 
+# Microsoft Disclaimer for custom scripts
+# ================================================================================================================
+# The sample scripts are not supported under any Microsoft standard support program or service. The sample scripts
+# are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, 
+# without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire
+# risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event
+# shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be
+# liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business
+# interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to 
+# use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
+# ================================================================================================================
+#
+##################################################################################################################
+#>
+
+#Get current working directory
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+#Paste below your Tenant ID, App ID and App Secret (App key).
+$tenantId = '' ### Paste your tenant ID here
+$appId = '' ### Paste your Application ID here
+$appSecret = '' ### Paste your Application secret here
+
+$resourceAppIdUri = 'https://graph.windows.net'
+$oAuthUri = "https://login.windows.net/$TenantId/oauth2/token"
+$authBody = [Ordered] @{
+    resource = "$resourceAppIdUri"
+    client_id = "$appId"
+    client_secret = "$appSecret"
+    grant_type = 'client_credentials'
+}
+
+#call API
+$authResponse = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $authBody -ErrorAction Stop
+$authResponse
+Out-File -FilePath "$scriptDir\LatestSIEM-token.txt" -InputObject $token
