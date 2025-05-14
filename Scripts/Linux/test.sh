e4b4c9e83406bce1c2889efcfe4c9fdba8f524af
JesseEsquivel <33558203+JesseEsquivel@users.noreply.github.com>
2025-05-14 16:33:04 -0400

    Update test.sh

--- Scripts/Linux/test.sh
+++ Scripts/Linux/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+##################################################################################################################
 checkFiles() {
 #find onboarding zip and unzip
 currentDir=$(pwd)
 zipFile=$(find "$currentDir" -maxdepth 1 -name "Gateway*.zip" | head -n 1)
 if [ -n "$zipFile" ]; then
 	echo "Found correct onboarding package and unzipped in current location!"
-	unzip -j $zipFile
+	unzip -j "$zipFile"
 else
 	echo "No onboarding .zip file from portal found in current directory. Make sure to place onboarding package zip file in this folder, exiting..."
 	exit 2
@@ -14,7 +15,7 @@ fi
 #check that zip extracted successfully
 if [[ ! -f MicrosoftDefenderATPOnboardingLinuxServer.py ]]
 then
-	printf "%10s%-s\n" "" "Onboarding file [MicrosoftDefenderATPOnboardingLinuxServer.py] NOT found. Check that onboarding zip is in current directory and was extracted! ?"
+	printf "%10s%-s\n" "" "Onboarding file [MicrosoftDefenderATPOnboardingLinuxServer.py] NOT found. Check that onboarding zip is in current directory and was extracted!"
 	exit 2
 else
 	echo "Linux onboarding file found!"
@@ -23,7 +24,7 @@ fi
 #make sure Fastpath blob is present
 fpName="FpTrustAnchors_Signed.db"
 fpPath="$currentDir/$fpName"
-if [ -n "$fpPath" ]; then
+if [ -f "$fpPath" ]; then
 	echo "Found required onboarding blob!"
 else
 	echo "Missing required onboarding blob file: '$fpName' ensure it is present in this directory, exiting..."
@@ -33,7 +34,7 @@ fi
 #check for mdatp_managed.json
 managedFile="mdatp_managed.json"
 managedPath="$currentDir/$managedFile"
-if [ -n "$managedPath" ]; then
+if [ -f "$managedPath" ]; then
 	echo "Mdatp Managed json file is present, can configure Engine"
 else
 	echo "Mdatp Managed json file is NOT present: '$managedPath' cannot configure engine, ensure managed json file is present this directory, exiting..."
